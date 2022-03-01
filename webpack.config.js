245a1396a4ba8a22b72f69621a45dcfea62874b7
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-03-01 13:30:00 -0600

    Html loader and plugin was installed and set

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -17,12 +18,25 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
     devServer: {
         static: path.join(__dirname, 'dist'),
         compress: true,
-        port: 3006
+        port: 3006,
+        open: true
     }
 }
